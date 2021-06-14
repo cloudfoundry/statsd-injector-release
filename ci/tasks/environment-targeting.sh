4bfc7e4720e8ec9fb2627006a6bf9ad2540ebf75
Renee Chu <reneec@vmware.com>
2021-06-14 18:12:52 +0000

    Delete `ci` directory
    * pipeline now moved to `https://github.com/pivotal/tas-runtime`

--- ci/tasks/environment-targeting.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-API_URL=$(cat toolsmiths-env/metadata | jq -r '.cf.api_url')
-eval "$(bbl print-env --metadata-file toolsmiths-env/metadata)"
-password=$(credhub get --name=$(credhub find | grep cf_admin | awk '{print $3}') --output-json | jq -r .value)
-cf api $API_URL --skip-ssl-validation
-cf auth admin $password
-cf target -o system
